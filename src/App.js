c67ae49722c227e90526e9c5d2417700d2f80dd6
reqLane <vlad5000191@gmail.com>
2022-12-15 05:52:48 +0200

    11

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, {useState} from "react";
 import {Navigate, Route, Routes} from "react-router";
 import LoginForm from "./components/LoginForm";
 import Dashboard from "./components/Dashboard";
+import useCookie from "react-use-cookie";
 
 function App() {
     const [user, setUser] = useState("");
     const [error, setError] = useState("");
 
+    const [userToken, setUserToken] = useCookie('loginKey', '');
+
     const Login = details => {
         let loginRegExp = new RegExp(/[a-zA-Z]+[1-9]/);
         if(loginRegExp.test(details.login)) {
             setUser(details.login);
+            setUserToken(details.login, {
+                days: 30,
+                Secure: true,
+            })
             setError("");
         }
         else {
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import {Navigate} from "react-router";
-import useCookie from 'react-use-cookie'
 
 function LoginForm({Login, error, authorized}) {
     if(authorized) return (<Navigate to="/dashboard" />);
 
     const [details, setDetails] = useState({login: "", password: ""});
-
-    const [userToken, setUserToken] = useCookie();
 
     const submitFunc = e => {
         e.preventDefault();
